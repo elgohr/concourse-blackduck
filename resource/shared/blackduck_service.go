472c70ff14112c4964daf687d852cbf3abf4347f
Hasselbring, Hannes <hannes.hasselbring@volkswagen.de>
2019-04-26 15:22:16 +0200

    added get Blackduck auth token query function and test

--- resource/shared/blackduck_service.go
+++ resource/shared/blackduck_service.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"sort"
+	"strings"
 )
 
 const ProjectCacheName = "./project.cache"
@@ -64,6 +66,45 @@ func (b *Blackduck) GetProjectVersions(project *Project) ([]Ref, error) {
 		refs = append(refs, Ref{Ref: versionRef})
 	}
 	return refs, nil
+}
+
+func (b *Blackduck) GetAuthenticationToken(blackBaseDuckUrl string, blackDuckUsername string, blackDuckPassword string) (string, error) {
+
+	const urlLoginPostfix = "/j_spring_security_check"
+	const formUserName= "j_username"
+	const formPassword = "j_password"
+
+	res, err := http.PostForm(blackBaseDuckUrl+urlLoginPostfix,
+		url.Values{formUserName: {blackDuckUsername}, formPassword: {blackDuckPassword}})
+
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	if isStatusCodeNotOk(res) {
+		return "", errors.New("authentication failed")
+	}
+
+	cookieHeader := res.Header.Get("Set-Cookie")
+
+	cookieHeaderEntries := strings.Split(cookieHeader, ";")
+
+	var token string
+	for _, cookieHeaderEntry := range cookieHeaderEntries {
+		trimmedCookieHeaderEntry := strings.TrimSpace(cookieHeaderEntry)
+		const tokenPrefix = "AUTHORIZATION_BEARER="
+		if strings.HasPrefix(trimmedCookieHeaderEntry, tokenPrefix) {
+			token = strings.Replace(trimmedCookieHeaderEntry, tokenPrefix, "", 1)
+			return token, nil
+		}
+	}
+
+	return "", errors.New("token not found")
+}
+
+func isStatusCodeNotOk(res *http.Response) bool {
+	return res.StatusCode >= 300
 }
 
 func writeProjectToCache(project Project) {
